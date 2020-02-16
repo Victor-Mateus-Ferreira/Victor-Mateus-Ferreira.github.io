58c72b33ff3a0c418d0c67e210c16a8e191c4e16
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-02-16 12:07:12 -0300

    Exercicio 8 - True ou False para numero par ou impar

---
+++ exercises/4_1/exercise_8.js
@@ -0,0 +1,17 @@
+
+let numberOne = 5;
+
+let numberTwo = 8;
+
+let numberTree = 11;
+
+if (numberOne % 2 == 0 || numberTwo % 2 == 0 || numberTree % 2 == 0) {
+
+    console.log("true");
+}
+
+else {
+
+    console.log("false");
+}
+  
