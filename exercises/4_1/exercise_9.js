6213cd1b209edc7f5620f2d021bf2217f255449c
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-02-16 12:15:23 -0300

    Exercicio 9 - True ou False para numero impar

---
+++ exercises/4_1/exercise_9.js
@@ -0,0 +1,16 @@
+let numberOne = 56;
+
+let numberTwo = 8;
+
+let numberTree = 10;
+
+if (numberOne % 2 == 1 || numberTwo % 2 == 1 || numberTree % 2 == 1) {
+
+    console.log("true");
+}
+
+else {
+
+    console.log("false");
+}
+  
