fad3cd334348d5f553bfa39445b19c7b220a8634
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-03-16 19:29:32 -0300

    adicionando função click

---
+++ exercises/7_3/script.js
@@ -0,0 +1,13 @@
+const carta = document.getElementById('cartaVirada');
+const cartaSurpresa = []
+
+carta.addEventListener ('click', virada);
+
+cartaAleatoria = '';
+
+function virada (){
+    for (let i=0; i <= 7; i++) {
+        cartaAleatoria[i]= 
+    }
+    // console.log("olá");
+}
